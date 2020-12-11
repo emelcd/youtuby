d8b5dd20ee2f0a7efd54b446862b131503c026d9
unknown <mick.altura@gmail.com>
2020-12-11 04:15:08 +0100

    Implementados Vídeo/Música

--- youtuby.py
+++ youtuby.py
@@ -15,11 +15,13 @@ try:
     open("./ffmpeg.exe")
 except:
     print(Fore.RED+"DESCARGANDO ARCHIVOS PARA LA EXTRACCIÓN Y CONVERSIÓN DEL AUDIO")
-    urllib.request.urlretrieve("https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2020-12-10-12-59/ffmpeg-N-100388-g412c3b37a4-win64-gpl.zip", "ffmpeg.zip")
+    urllib.request.urlretrieve(
+        "https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2020-12-10-12-59/ffmpeg-N-100388-g412c3b37a4-win64-gpl.zip", "ffmpeg.zip")
 
     with ZipFile('ffmpeg.zip', 'r') as zipO:
         zipO.extract('ffmpeg-N-100388-g412c3b37a4-win64-gpl/bin/ffmpeg.exe')
-    shutil.move('.\\ffmpeg-N-100388-g412c3b37a4-win64-gpl\\bin\\ffmpeg.exe', '.\\')
+    shutil.move(
+        '.\\ffmpeg-N-100388-g412c3b37a4-win64-gpl\\bin\\ffmpeg.exe', '.\\')
     shutil.rmtree('.//ffmpeg-N-100388-g412c3b37a4-win64-gpl')
     os.remove('ffmpeg.zip')
 
@@ -32,9 +34,10 @@ win32clipboard.EmptyClipboard()
 win32clipboard.SetClipboardText("#Start")
 win32clipboard.CloseClipboard()
 
-colors = [Fore.WHITE, Fore.YELLOW, Fore.RED, Fore.LIGHTBLUE_EX, Fore.LIGHTMAGENTA_EX, Fore.LIGHTGREEN_EX, Fore.LIGHTRED_EX]
+colors = [Fore.WHITE, Fore.YELLOW, Fore.RED, Fore.LIGHTBLUE_EX,
+          Fore.LIGHTMAGENTA_EX, Fore.LIGHTGREEN_EX, Fore.LIGHTRED_EX]
 
-youtube_logo ="""
+youtube_logo = """
  ▄· ▄▌      ▄• ▄▌▄▄▄▄▄▄• ▄▌▄▄▄▄·  ▄· ▄▌
 ▐█▪██▌▪     █▪██▌•██  █▪██▌▐█ ▀█▪▐█▪██▌
 ▐█▌▐█▪ ▄█▀▄ █▌▐█▌ ▐█.▪█▌▐█▌▐█▀▀█▄▐█▌▐█▪
@@ -56,7 +59,7 @@ while True:
     if data != clipis[-1] and "youtube" in data:
         clipis.append(data)
         win32clipboard.CloseClipboard()
-    elif data == "fin" or data == "end" or data=="exit":
+    elif data == "fin" or data == "end" or data == "exit":
         break
     else:
         print(str(len(clipis) - 1) + " enlaces copiados", end="\r")
@@ -68,15 +71,31 @@ fs = open("%s.txt" % folder, "w+")
 for clip in clipis:
     fs.write(clip+"\n")
 fs.close()
-print(Fore.LIGHTBLUE_EX)
 os.system('mkdir %s' % folder)
-print(Fore.LIGHTWHITE_EX)
 
-os.system('python -m youtube_dl -a %s.txt -x -i --audio-format "mp3" ' % folder)
-os.system('move *.mp3 ./%s' % folder)
+menu_P = """
+/*  o8ooooooooooooooooo8o  */
+/* (_) """+Fore.LIGHTWHITE_EX+"""1.Son Vídeos"""+Fore.LIGHTGREEN_EX+"""    (_) */
+/* (_) """+Fore.LIGHTWHITE_EX+"""2.Son Canciones (_)"""+Fore.LIGHTGREEN_EX+""" */
+/*  O8ooooooooooooooooo8O  */
+"""
+lock = True
+while lock:
+    print(Fore.LIGHTGREEN_EX + menu_P)
+    selection = input("\n")
+    if selection == "2":
+        os.system(
+            'python -m youtube_dl -a %s.txt -x -i --audio-format "mp3" ' % folder)
+        os.system('move *.mp3 ./%s' % folder)
+        lock = False
+
+    elif selection == "1":
+        os.system(
+            'python -m youtube_dl -a %s.txt -i -f "mp4" ' % folder)
+        os.system('move *.mp4 ./%s' % folder)
+        lock = False
+    else:
+        print("Pulse 1 o 2")
+
 os.system('DEL %s.txt /F' % folder)
-
 print(Fore.LIGHTMAGENTA_EX + "\nLA DESCARGA HA ACABADO\n")
-
-def menu():
-    print("ass")
